557525eab8273be650e1f12b3aa6155b7ee488db
Cláudio Silva <claudio.silva@impactwave.com>
2015-05-13 18:53:40 +0100

    Completed the file-upload remake.

--- src/FileUpload.php
+++ src/FileUpload.php
@@ -12,6 +12,7 @@ class FileUploadAttributes extends ComponentAttributes
   public $value;
   public $no_clear = false;
   public $disabled = false;
+  public $clear_button_class = 'fa fa-times';
 
   protected function typeof_name ()
   {
@@ -31,6 +32,11 @@ class FileUploadAttributes extends ComponentAttributes
   protected function typeof_disabled ()
   {
     return AttributeType::BOOL;
+  }
+
+  protected function typeof_clear_button_class ()
+  {
+    return AttributeType::TEXT;
   }
 }
 
@@ -70,52 +76,57 @@ class FileUpload extends VisualComponent
   protected function render ()
   {
     $this->page->enableFileUpload = true;
-    $attr  = $this->attrs ();
-    $value = $attr->get ('value', '');
+    $attr                         = $this->attrs ();
+    $value                        = $attr->get ('value', '');
+    $id = $attr->id;
+    $name = $attr->name;
 
     if ($this->autoId)
       $this->setAutoId ();
     $this->beginTag ('input');
-    $this->addAttribute ('id', $attr->id . (empty($value) ? 'File' : 'Text'));
+    $this->addAttribute ('id', $id . (empty($value) ? 'File' : 'Text'));
     $this->addAttribute ('class', enum (' ',
       $this->className,
       $this->cssClassName,
       $this->attrs ()->class,
-      $this->attrs ()->css_class,
       $this->attrs ()->disabled ? 'disabled' : null
     ));
     if (!empty($this->attrs ()->html_attrs))
       echo ' ' . $this->attrs ()->html_attrs;
 
-    if (!empty($value)) {
+    if (empty($value)) {
+      // File doesn't exist
+
+      $this->addAttribute ('type', 'file');
+      $this->addAttribute ('name', "{$name}_file");
+      $this->endTag ();
+    }
+    else {
       // File exists
 
       $this->addAttribute ('type', 'text');
       $this->addAttribute ('value', Media::getOriginalFileName ($value));
       $this->addAttribute ('readonly', "");
 
+      $this->addTag ('button', [
+        'class' => "btn btn-default $attr->clear_button_class",
+        'onclick' => "$('#{$id}Field').val('');var c=$('#{$id}Text').hide().attr('class');$('#{$id}File').show().attr('class',c);$(this).hide()"
+      ]);
+
       $this->beginTag ('input');
       $this->addAttribute ('type', 'file');
-      $this->addAttribute('style', 'display: none');
-      $this->addAttribute ('id', "{$attr->id}File");
-      $this->endTag ();
-      $this->addTag ('button', [
-        'onclick' => "$('#{$attr->id}Field').val('');$('#{$attr->id}Text').hide();$('#{$attr->id}File').show();$(this).hide()"
-      ], 'Clear');
-
-    } else {
-      // File doesn't exist
-
-      $this->addAttribute ('type', 'file');
+      $this->addAttribute ('name', "{$name}_file");
+      $this->addAttribute ('style', 'display: none');
+      $this->addAttribute ('id', "{$id}File");
       $this->endTag ();
     }
 
     $this->beginTag ('input');
     $this->addAttribute ('type', 'hidden');
-    $this->addAttribute ('id', "{$attr->id}Field");
-    if (isset($attr->name))
-      $this->addAttribute ('name', $attr->name);
-    else $this->addAttribute ('name', $attr->id);
+    $this->addAttribute ('id', "{$id}Field");
+    if (isset($name))
+      $this->addAttribute ('name', $name);
+    else $this->addAttribute ('name', $id);
     $this->addAttribute ('value', $value);
     $this->endTag ();
     $this->handleFocus ();
