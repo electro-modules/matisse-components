8867c30245c4b5e6acdedbde8cf07459aa7fc6fa
Cláudio Silva <claudio.silva@impactwave.com>
2015-07-09 12:55:28 +0100

    The package now registers its own tags if used on Selene.

---
+++ bootstrap.php
@@ -0,0 +1,27 @@
+<?php
+
+ModuleOptions (__DIR__, [
+  'components' => [
+    'Button'      => 'Selene\Matisse\Components\Button',
+    'Calendar'    => 'Selene\Matisse\Components\Calendar',
+    'Checkbox'    => 'Selene\Matisse\Components\Checkbox',
+    'DataGrid'    => 'Selene\Matisse\Components\DataGrid',
+    'Field'       => 'Selene\Matisse\Components\Field',
+    'FileUpload'  => 'Selene\Matisse\Components\FileUpload',
+    'Head'        => 'Selene\Matisse\Components\Head',
+    'HtmlRditor'  => 'Selene\Matisse\Components\HtmlEditor',
+    'Image'       => 'Selene\Matisse\Components\Image',
+    'ImageField'  => 'Selene\Matisse\Components\ImageField',
+    'Input'       => 'Selene\Matisse\Components\Input',
+    'Label'       => 'Selene\Matisse\Components\Label',
+    'Link'        => 'Selene\Matisse\Components\Link',
+    'MainMenu'    => 'Selene\Matisse\Components\MainMenu',
+    'Model'       => 'Selene\Matisse\Components\Model',
+    'Paginator'   => 'Selene\Matisse\Components\Paginator',
+    'RadioButton' => 'Selene\Matisse\Components\Radiobutton',
+    'Selector'    => 'Selene\Matisse\Components\Selector',
+    'Tab'         => 'Selene\Matisse\Components\Tab',
+    'TabPage'     => 'Selene\Matisse\Components\TabPage',
+    'Tabs'        => 'Selene\Matisse\Components\Tabs',
+  ],
+]);
