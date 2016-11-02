cbd8886bc0a0b55819494d222f33b3d70506527b
Cláudio Silva <claudio.silva@impactwave.com>
2016-11-02 18:43:59 +0000

    FIX: bootstrapping upgrade.

--- MatisseComponents/Config/MatisseComponentsModule.php
+++ MatisseComponents/Config/MatisseComponentsModule.php
@@ -4,6 +4,7 @@ namespace Electro\Plugins\MatisseComponents\Config;
 use Electro\ContentServer\Config\ContentServerSettings;
 use Electro\Core\Assembly\ModuleInfo;
 use Electro\Core\Assembly\Services\Bootstrapper;
+use Electro\Core\Profiles\WebProfile;
 use Electro\Interfaces\DI\InjectorInterface;
 use Electro\Interfaces\ModelControllerInterface;
 use Electro\Interfaces\ModuleInterface;
@@ -18,53 +19,54 @@ class MatisseComponentsModule implements ModuleInterface
 {
   static function bootUp (Bootstrapper $bootstrapper, ModuleInfo $moduleInfo)
   {
-    $bootstrapper->on (CONFIGURE,
-      function (MatisseSettings $matisseSettings, ModelControllerInterface $modelController,
-                InjectorInterface $injector, ContentServerSettings $contentServerSettings)
-      use ($moduleInfo) {
-        $matisseSettings
-          ->registerMacros ($moduleInfo)
-          ->registerComponents ([
-            'Button'         => C\Button::class,
-            'Checkbox'       => C\Checkbox::class,
-            'DataGrid'       => C\DataGrid::class,
-            'Dropzone'       => C\Dropzone::class,
-            'Field'          => C\Field::class,
-            'FileUpload'     => C\FileUpload::class,
-            'HtmlEditor'     => C\HtmlEditor::class,
-            'Image'          => C\Image::class,
-            'ImageField'     => C\ImageField::class,
-            'Input'          => C\Input::class,
-            'Label'          => C\Label::class,
-            'Link'           => C\Link::class,
-            'MainMenu'       => C\MainMenu::class,
-            'NavigationPath' => C\NavigationPath::class,
-            'Paginator'      => C\Paginator::class,
-            'RadioButton'    => C\RadioButton::class,
-            'Select'         => C\Select::class,
-            'Switch'         => C\Switch_::class,
-            'Tab'            => C\Tab::class,
-            'TabPage'        => C\TabPage::class,
-            'Tabs'           => C\Tabs::class,
-          ])
-          ->registerAssets ($moduleInfo, [
-            'dist/components.css',
-          ]);
+    if ($bootstrapper->profile instanceof WebProfile)
+      $bootstrapper->on (CONFIGURE,
+        function (MatisseSettings $matisseSettings, ModelControllerInterface $modelController,
+                  InjectorInterface $injector, ContentServerSettings $contentServerSettings)
+        use ($moduleInfo) {
+          $matisseSettings
+            ->registerMacros ($moduleInfo)
+            ->registerComponents ([
+              'Button'         => C\Button::class,
+              'Checkbox'       => C\Checkbox::class,
+              'DataGrid'       => C\DataGrid::class,
+              'Dropzone'       => C\Dropzone::class,
+              'Field'          => C\Field::class,
+              'FileUpload'     => C\FileUpload::class,
+              'HtmlEditor'     => C\HtmlEditor::class,
+              'Image'          => C\Image::class,
+              'ImageField'     => C\ImageField::class,
+              'Input'          => C\Input::class,
+              'Label'          => C\Label::class,
+              'Link'           => C\Link::class,
+              'MainMenu'       => C\MainMenu::class,
+              'NavigationPath' => C\NavigationPath::class,
+              'Paginator'      => C\Paginator::class,
+              'RadioButton'    => C\RadioButton::class,
+              'Select'         => C\Select::class,
+              'Switch'         => C\Switch_::class,
+              'Tab'            => C\Tab::class,
+              'TabPage'        => C\TabPage::class,
+              'Tabs'           => C\Tabs::class,
+            ])
+            ->registerAssets ($moduleInfo, [
+              'dist/components.css',
+            ]);
 
-        $modelController
-          ->registerExtension ($injector->makeFactory (ImageFieldHandler::class));
+          $modelController
+            ->registerExtension ($injector->makeFactory (ImageFieldHandler::class));
 
-        File::deleting (function (File $model) use ($contentServerSettings, $injector) {
-          if (exists ($model->path)) {
-            $path = "{$contentServerSettings->fileArchivePath()}/$model->path";
-            if (file_exists ($path))
-              unlink ($path);
-            $glideServer = $injector->make (Server::class);
-            $glideServer->deleteCache ($model->path);
-          }
+          File::deleting (function (File $model) use ($contentServerSettings, $injector) {
+            if (exists ($model->path)) {
+              $path = "{$contentServerSettings->fileArchivePath()}/$model->path";
+              if (file_exists ($path))
+                unlink ($path);
+              $glideServer = $injector->make (Server::class);
+              $glideServer->deleteCache ($model->path);
+            }
+          });
+
         });
-
-      });
   }
 
 }
