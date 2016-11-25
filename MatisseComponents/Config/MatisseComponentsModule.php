7ade80cecdabef4725f65b3aa6d2bed9834efe5e
Cláudio Silva <claudio.silva@impactwave.com>
2016-11-25 22:56:24 +0000

    New bootstrapping rules and changes to configuration profiles (getCompatibleProfiles).

--- MatisseComponents/Config/MatisseComponentsModule.php
+++ MatisseComponents/Config/MatisseComponentsModule.php
@@ -16,56 +16,60 @@ use League\Glide\Server;
 
 class MatisseComponentsModule implements ModuleInterface
 {
+  static function getCompatibleProfiles ()
+  {
+    return [WebProfile::class];
+  }
+
   static function startUp (KernelInterface $kernel, ModuleInfo $moduleInfo)
   {
-    if ($kernel->getProfile () instanceof WebProfile)
-      $kernel->onConfigure (
-        function (MatisseSettings $matisseSettings, ModelControllerInterface $modelController,
-                  InjectorInterface $injector, ContentServerSettings $contentServerSettings)
-        use ($moduleInfo) {
-          $matisseSettings
-            ->registerMacros ($moduleInfo)
-            ->registerComponents ([
-              'Button'         => C\Button::class,
-              'Checkbox'       => C\Checkbox::class,
-              'DataGrid'       => C\DataGrid::class,
-              'Dropzone'       => C\Dropzone::class,
-              'Field'          => C\Field::class,
-              'FileUpload'     => C\FileUpload::class,
-              'HtmlEditor'     => C\HtmlEditor::class,
-              'Image'          => C\Image::class,
-              'ImageField'     => C\ImageField::class,
-              'Input'          => C\Input::class,
-              'Label'          => C\Label::class,
-              'Link'           => C\Link::class,
-              'MainMenu'       => C\MainMenu::class,
-              'NavigationPath' => C\NavigationPath::class,
-              'Paginator'      => C\Paginator::class,
-              'RadioButton'    => C\RadioButton::class,
-              'Select'         => C\Select::class,
-              'Switch'         => C\Switch_::class,
-              'Tab'            => C\Tab::class,
-              'TabPage'        => C\TabPage::class,
-              'Tabs'           => C\Tabs::class,
-            ])
-            ->registerAssets ($moduleInfo, [
-              'dist/components.css',
-            ]);
+    $kernel->onConfigure (
+      function (MatisseSettings $matisseSettings, ModelControllerInterface $modelController,
+                InjectorInterface $injector, ContentServerSettings $contentServerSettings)
+      use ($moduleInfo) {
+        $matisseSettings
+          ->registerMacros ($moduleInfo)
+          ->registerComponents ([
+            'Button'         => C\Button::class,
+            'Checkbox'       => C\Checkbox::class,
+            'DataGrid'       => C\DataGrid::class,
+            'Dropzone'       => C\Dropzone::class,
+            'Field'          => C\Field::class,
+            'FileUpload'     => C\FileUpload::class,
+            'HtmlEditor'     => C\HtmlEditor::class,
+            'Image'          => C\Image::class,
+            'ImageField'     => C\ImageField::class,
+            'Input'          => C\Input::class,
+            'Label'          => C\Label::class,
+            'Link'           => C\Link::class,
+            'MainMenu'       => C\MainMenu::class,
+            'NavigationPath' => C\NavigationPath::class,
+            'Paginator'      => C\Paginator::class,
+            'RadioButton'    => C\RadioButton::class,
+            'Select'         => C\Select::class,
+            'Switch'         => C\Switch_::class,
+            'Tab'            => C\Tab::class,
+            'TabPage'        => C\TabPage::class,
+            'Tabs'           => C\Tabs::class,
+          ])
+          ->registerAssets ($moduleInfo, [
+            'dist/components.css',
+          ]);
 
-          $modelController
-            ->registerExtension ($injector->makeFactory (ImageFieldHandler::class));
+        $modelController
+          ->registerExtension ($injector->makeFactory (ImageFieldHandler::class));
 
-          File::deleting (function (File $model) use ($contentServerSettings, $injector) {
-            if (exists ($model->path)) {
-              $path = "{$contentServerSettings->fileArchivePath()}/$model->path";
-              if (file_exists ($path))
-                unlink ($path);
-              $glideServer = $injector->make (Server::class);
-              $glideServer->deleteCache ($model->path);
-            }
-          });
-
+        File::deleting (function (File $model) use ($contentServerSettings, $injector) {
+          if (exists ($model->path)) {
+            $path = "{$contentServerSettings->fileArchivePath()}/$model->path";
+            if (file_exists ($path))
+              unlink ($path);
+            $glideServer = $injector->make (Server::class);
+            $glideServer->deleteCache ($model->path);
+          }
         });
+
+      });
   }
 
 }
