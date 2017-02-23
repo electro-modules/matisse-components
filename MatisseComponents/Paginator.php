5f04fcb5c85ca706c5dc2491da63fa9c87fd9884
Cláudio Silva <claudio.silva@impactwave.com>
2017-02-23 04:33:40 +0000

    Upgrade Paginator.

--- MatisseComponents/Paginator.php
+++ MatisseComponents/Paginator.php
@@ -1,11 +1,26 @@
 <?php
+
 namespace Electro\Plugins\MatisseComponents;
 
+use Electro\Interfaces\Http\Shared\CurrentRequestInterface;
 use Matisse\Components\Base\HtmlComponent;
 use Matisse\Properties\Base\HtmlComponentProperties;
 
 class PaginatorProperties extends HtmlComponentProperties
 {
+  /**
+   * @var string
+   */
+  public $itemClass = '';
+  /**
+   * @var string
+   */
+  public $linkClass = '';
+  /**
+   * @var string A sprintf-compatible format string, where the first parameter is the link URL, the second is either a
+   *             '&' or a '?', and the third is the page number.
+   */
+  public $linkTemplate = '%s%sp=%d';
   /**
    * @var int
    */
@@ -15,13 +30,13 @@ class PaginatorProperties extends HtmlComponentProperties
    */
   public $pageCount = 0;
   /**
+   * @var bool Show &lt;&lt; and >> buttons even when disabled?
+   */
+  public $showDisabled = false;
+  /**
    * @var int
    */
   public $total = 0;
-  /**
-   * @var string
-   */
-  public $uri = '';
 }
 
 class Paginator extends HtmlComponent
@@ -32,6 +47,16 @@ class Paginator extends HtmlComponent
   public $props;
 
   protected $containerTag = 'nav';
+  /**
+   * @var CurrentRequestInterface
+   */
+  private $request;
+
+  public function __construct (CurrentRequestInterface $request)
+  {
+    parent::__construct ();
+    $this->request = $request;
+  }
 
   protected function postRender ()
   {
@@ -49,45 +74,54 @@ class Paginator extends HtmlComponent
   {
     $prop = $this->props;
 
-    $SIZE  = floor (($prop->pageCount - 1) / 2);
-    $page  = $prop->page;
-    $total = $prop->total;
+    $linkTpl = function ($uri, $sep, $page) {
+      return sprintf ($this->props->linkTemplate, $uri, $sep, $page);
+    };
+    $SIZE    = floor (($prop->pageCount - 1) / 2);
+    $page    = $prop->page;
+    $total   = $prop->total;
     if ($total < 2) return;
-    $uri   = $prop->uri;
+    $uri   = preg_replace ('/&p=\d+/', '', $this->request->getUri ());
+    $sep   = strpos ($uri, '?') === false ? '?' : '&';
     $start = $page - $SIZE;
     $end   = $start + 2 * $SIZE;
     if ($start < 1) {
-      $d = -$start + 1;
+      $d     = -$start + 1;
       $start += $d;
-      $end += $d;
+      $end   += $d;
     }
     if ($end > $total) {
-      $d = $end - $total;
-      $end -= $d;
+      $d     = $end - $total;
+      $end   -= $d;
       $start -= $d;
       if ($start < 1) $start = 1;
     }
     $this->begin ('ul');
     $this->attr ('class', 'pagination');
-    if ($start > 1) {
+    if ($start > 1 || $prop->showDisabled) {
       $st = $start - 1;
       if ($st < 1) $st = 1;
-      $this->begin ('li');
-      $this->tag ('a', ['href' => "$uri?p=$st", 'class' => 'prev'], '&laquo;');
+      $this->begin ('li', ['class' => ($start > 1 ? '' : 'disabled ') . $prop->itemClass]);
+      $this->tag ('a',
+        ['href' => $start > 1 ? $linkTpl($uri, $sep, $st) : 'javascript:void(0)', 'class' => "prev $prop->linkClass"],
+        '&laquo;');
       $this->end ();
       $this->beginContent ();
     }
     for ($n = $start; $n <= $end; ++$n) {
-      $this->begin ('li', ['class' => $n == $page ? 'active' : '']);
-      $this->tag ('a', ['href' => "$uri?p=$n"], $n);
+      $this->begin ('li', ['class' => ($n == $page ? 'active' : '') . ' ' . $prop->itemClass]);
+      $this->tag ('a', ['href' => $linkTpl($uri, $sep, $n), 'class' => $prop->linkClass], $n);
       $this->end ();
     }
-    if ($end < $total) {
+    if ($end < $total || $prop->showDisabled) {
       $this->beginContent ();
       $ed = $end + 1;
       if ($ed > $total) $ed = $total;
-      $this->begin ('li');
-      $this->tag ('a', ['href' => "$uri?p=$ed", 'class' => 'next'], '&raquo;');
+      $this->begin ('li', ['class' => ($end < $total ? '' : 'disabled ') . $prop->itemClass]);
+      $this->tag ('a',
+        ['href'  => $end < $total ? $linkTpl($uri, $sep, $ed) : 'javascript:void(0)', 'class' => "next $prop->linkClass",
+        ],
+        '&raquo;');
       $this->end ();
     }
     $this->end ();
